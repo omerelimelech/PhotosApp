67737dc1a93dc4844a0add022f7cfdc012aa4ead
Omer Elimelech <omer.tkd@gmail.com>
2020-02-15 13:18:28 +0200

    init

---
+++ PhotosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by Omer Elimelech on 20 Shevat 5780.
+//  Copyright © 5780 Omer Elimelech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("init")
+    }
+
+
+}
+
